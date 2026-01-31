a1d6de41da45b11990418fc98186fbbd7fca6aca
meet2op <71132738+meet2op@users.noreply.github.com>
2026-01-31 13:16:11 +0530

    Refactor background.js to remove default block settings
    Removed applyDefaultBlock function and related calls.

--- background.js
+++ background.js
@@ -1,23 +1,5 @@
-const BLOCK = "block";
-
-function applyDefaultBlock() {
-  chrome.contentSettings.camera.set({
-    primaryPattern: "<all_urls>",
-    setting: BLOCK
-  });
-
-  chrome.contentSettings.microphone.set({
-    primaryPattern: "<all_urls>",
-    setting: BLOCK
-  });
-
-  chrome.contentSettings.geolocation.set({
-    primaryPattern: "<all_urls>",
-    setting: BLOCK
-  });
-}
-
+// background.js (Manifest V3 safe)
 chrome.runtime.onInstalled.addListener(() => {
-  applyDefaultBlock();
+  console.log("Permission Firewall installed");
 });
 
